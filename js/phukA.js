6d82bd4df5f9fe85bf9beade0ead21a0c196809a
mxmadu <157181355+mxmadu@users.noreply.github.com>
2024-07-29 11:48:39 +0100

    Update phukA.js

--- js/phukA.js
+++ js/phukA.js
@@ -63,7 +63,7 @@ scene.add(dLight);
 
 const loader = new GLTFLoader(loadingManager);
 let model;
-loader.load('../models/PHUK_PHACE_A.glb', function(gltf) {
+loader.load('https://mxmadu.github.io/pretty-hurts/models/PHUK_PHACE_A.glb', function(gltf) {
   model = gltf.scene;
   model.rotation.set(0, Math.PI, 0);
   model.receiveShadow = true;
